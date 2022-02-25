6f768ce9597871fa7806f359c2f13ad27e989087
jasminkorat0078 <99074297+jasminkorat0078@users.noreply.github.com>
2022-02-25 16:41:35 +0530

    task12

--- localstorageobject.js
+++ localstorageobject.js
@@ -3,26 +3,44 @@ var form = document.getElementById("my-form");
 
 form.addEventListener("submit",getdata);
 
-var us = document.getElementById("user");
-
+// var us = document.getElementById("userdetail");
+// let i=0;
 function getdata(e){
     e.preventDefault();
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
+    var ulist=document.getElementById('userdetail');
+    ulist.innerHTML=''
 
     let obj = {
-        n:name,
-        em:email
-
+        name,
+        email
     };
-    let myobj=JSON.stringify(obj);
-    localStorage.setItem("userdetails",myobj);
-    
-    let getobj=JSON.parse(localStorage.getItem("userdetails"));
-    console.log(getobj);
 
-    us.innerHTML = "user details  :  " + getobj['n'] +"<br>" + "email is : "+ getobj['em'];
+    localStorage.setItem(obj.email,JSON.stringify(obj));
+    showonscreen(obj);
+}
 
-    
-    
+function showonscreen(user){
+    const parentnode = document.getElementById("userdetail")
+    const childnode = '<li>Username = ' + user.name + ' , Email id = '+ user.email+'</li>'
+    parentnode.innerHTML=parentnode.innerHTML+childnode;
+}
+
+document.addEventListener('DOMContentLoaded',refresh);
+function refresh(e){
+    e.preventDefault();
+    console.log("refresh")
+    let i=0;
+    for(i=0;i<localStorage.length;i++)
+    {
+        k=localStorage.key(i);
+        var getobj = JSON.parse(localStorage.getItem(k));
+        const parentnode = document.getElementById("userdetail")
+        const childnode = '<li>Username = ' + getobj['name'] + ' , Email id = '+ getobj
+        ['email']+'</li>'
+        
+        parentnode.innerHTML=parentnode.innerHTML+childnode;
+
+    }
 }
