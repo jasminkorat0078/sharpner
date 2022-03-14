1f113df0d2c28df6af30c0aef1e44c884244c8a6
jasminkorat0078 <99074297+jasminkorat0078@users.noreply.github.com>
2022-03-14 18:38:04 +0530

    get all user details while refreshing

--- crud.js
+++ crud.js
@@ -27,6 +27,31 @@ function getdata(e){
     //localStorage.setItem(obj.email,JSON.stringify(obj));
     
 }
+
+document.addEventListener('DOMContentLoaded',refresh);
+function refresh(e){
+    e.preventDefault();
+    console.log("refresh")
+    axios.get("https://crudcrud.com/api/ddc1dc13e6d04f7aac488adf716bfad8/user")
+        .then(res => {
+
+            for(var i=0;i<res.data.length;i++){
+                showonscreen(res.data[i]);
+            }
+        })
+    // for(i=0;i<localStorage.length;i++)
+    // {
+    //     k=localStorage.key(i);
+    //     var getobj = JSON.parse(localStorage.getItem(k));
+    //     const parentnode = document.getElementById("userdetail")
+    //     const childnode = '<li>Username = ' + getobj['name'] + ' , Email id = '+ getobj
+    //     ['email']+'</li>'
+        
+    //     parentnode.innerHTML=parentnode.innerHTML+childnode;
+
+    // }
+}
+
 function showonscreen(user){
     const parentnode = document.getElementById("userdetail")
     const childnode = `<li id = ${user.email}> Username = ' ${user.name}  , Email id = ' ${user.email}<button id='${user.email}' onclick="dlfn(${user.email})" style="float:center">delete</button></li>`
