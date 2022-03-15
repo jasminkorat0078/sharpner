9ca93227824bc889e91b47a4c6a92c82699813e4
jasminkorat0078 <99074297+jasminkorat0078@users.noreply.github.com>
2022-03-15 10:07:45 +0530

    crud update

--- crud.js
+++ crud.js
@@ -1,5 +1,5 @@
 var form = document.getElementById("my-form");
-
+var updatedata;
 
 
 
@@ -18,11 +18,16 @@ function getdata(e){
         name,
         email
     };
-    axios.post("https://crudcrud.com/api/ddc1dc13e6d04f7aac488adf716bfad8/user",obj).then(res => {
+    if (updatedata == false){
+        axios.post("https://crudcrud.com/api/ddc1dc13e6d04f7aac488adf716bfad8/user",obj).then(res => {
       console.log(res);
   }).catch(err =>console.error(err));
 
-    //showonscreen(obj);
+    }
+
+    
+
+    showonscreen(obj);
 
     //localStorage.setItem(obj.email,JSON.stringify(obj));
     
@@ -44,7 +49,8 @@ function refresh(e){
 
 function showonscreen(user){
     const parentnode = document.getElementById("userdetail")
-    const childnode = `<li id = ${user._id}> Username = ' ${user.name}'  , Email id = ' ${user.email}'<button id='${user._id}' onclick="dlfn('${user._id}')" style="float:center">delete</button></li>`
+    const childnode = `<li id = ${user._id}> Username = ' ${user.name}'  , Email id = ' ${user.email}'<button id='${user._id}' onclick="dlfn('${user._id}')" style="float:center">delete</button>
+    <button id='${user._id}' onclick="update('${user._id}')" style="float:center">update</button></li>`
     //const childnode = '<li id = '+user.email`> Username = ${user.name} , Email id = ${user.email}</li>`
     parentnode.innerHTML = parentnode.innerHTML + childnode;
     console.log(childnode);
@@ -77,4 +83,34 @@ function removechildnode(userid){
     }
     console.log(parentnode1);
   
+}
+
+function update(userid){
+    updatedata = true;
+    form.addEventListener("submit",getdatanew);
+
+// var us = document.getElementById("userdetail");
+// let i=0;
+    function getdatanew(e){
+        e.preventDefault();
+        var name = document.getElementById('name').value;
+        var email = document.getElementById('email').value;
+        var ulist=document.getElementById('userdetail');
+    //ulist.innerHTML=''
+
+        let newdata = {
+            name,
+            email
+        };
+    axios.put(`https://crudcrud.com/api/ddc1dc13e6d04f7aac488adf716bfad8/user/${userid}`,newdata)
+    .then(res =>console.log("updated"))
+    .catch(err => console.log("error"));
+    }
+    axios.get("https://crudcrud.com/api/ddc1dc13e6d04f7aac488adf716bfad8/user")
+        .then(res => {
+
+            for(var i=0;i<res.data.length;i++){
+                showonscreen(res.data[i]);
+            }
+        })
 }
