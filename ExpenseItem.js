b27466b3341553c2a73dc77ce935cac794915a60
jasminkorat0078 <99074297+jasminkorat0078@users.noreply.github.com>
2022-03-19 12:25:36 +0530

    Add evenlistener

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,17 +1,29 @@
+import Card from "../UI/card";
 import ExpenseDate  from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  var title = props.title;
+
+  const clickHandler = ()=>{
+    title = 'updated';
+    console.log(title);
+  }
+  const deleteHandler=()=>{
+    console.log("deleted");
+  }
  
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <h2>{props.location}</h2>
         <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickHandler}>change title</button>
+        <button onClick={deleteHandler}>Delete</button>
       </div>
-    </div>
+    </Card>
   );
 }
 
